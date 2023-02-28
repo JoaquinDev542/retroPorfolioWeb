e65f1c21ddb100533903cb67271177fdff58cddb
Joaquín Uriel García <82168815+JoaquinDev542@users.noreply.github.com>
2023-02-28 21:39:39 +0100

    upload files

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// Scroll Reveal
+// const sr = ScrollReveal({
+    // distance: '50px',
+    // duration: 1000,
+    // delay: 1000
+    // });
+    
+    // sr.reveal('.header', { origin: 'top' });
+    // sr.reveal('.hero' , {delay:200});
+    // sr.reveal('.skills' , {delay:300});
+    // sr.reveal('.aboutExtra' , {delay:200});
+    // sr.reveal('.aboutContent' , {delay:300});
+    // sr.reveal('.work--first' , {delay:200});
+    // sr.reveal('.work--second' , {delay:300});
+    // sr.reveal('.work--third' , {delay:500});   
