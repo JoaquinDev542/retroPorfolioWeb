b3298f3bc80ea32499e29a47156ffec510bf3227
Joaquín Uriel García <82168815+JoaquinDev542@users.noreply.github.com>
2023-03-01 21:19:56 +0100

    scroll reveal, work section

--- js/main.js
+++ js/main.js
@@ -1,15 +1,14 @@
 // Scroll Reveal
-// const sr = ScrollReveal({
+const sr = ScrollReveal({
     // distance: '50px',
-    // duration: 1000,
-    // delay: 1000
-    // });
+    duration: 1000,
+    delay: 200
+    });
     
-    // sr.reveal('.header', { origin: 'top' });
-    // sr.reveal('.hero' , {delay:200});
-    // sr.reveal('.skills' , {delay:300});
-    // sr.reveal('.aboutExtra' , {delay:200});
-    // sr.reveal('.aboutContent' , {delay:300});
-    // sr.reveal('.work--first' , {delay:200});
-    // sr.reveal('.work--second' , {delay:300});
-    // sr.reveal('.work--third' , {delay:500});   
+    sr.reveal('.header');
+    sr.reveal('.hero');
+    sr.reveal('.skills');
+    sr.reveal('.aboutExtra');
+    sr.reveal('.aboutContent');
+    sr.reveal('.works');
+console.log("aaa");
